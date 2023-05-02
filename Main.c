d2935098e67163bf1192b53b60b9eaa0b8439d6a
Norman Frieman <ezequiel-d.s@hotmail.com>
2023-05-02 20:30:07 -0300

    contar tempo de execução das funções

--- Main.c
+++ Main.c
@@ -1,13 +1,42 @@
+#include <time.h>
 #include "Headers/Arquivo.h"
 #include "Headers/Distancia.h"
 
 int main(int argc, char* argv[]){
+    clock_t start, stop;
+    start = clock();
+
     Entrada* info = Ler(argv[1]);
+    
+    stop = clock();
+
+    double time_taken = ((double) stop - start) / CLOCKS_PER_SEC;
+    printf("Ler: %.3f\n", time_taken);
+
+    // ######################
+
+    start = clock();
 
     Kruskel(info->D, info->P, info->QuantD, info->Contagem, atoi(argv[2]));
 
+    stop = clock();
+
+    time_taken = ((double) stop - start) / CLOCKS_PER_SEC;
+    printf("Kruskel: %.3f\n", time_taken);
+
+    // ######################
+    
+    start = clock();
+    
     Imprime(argv[3], info->P, info->Contagem);
 
+    stop = clock();
+
+    time_taken = ((double) stop - start) / CLOCKS_PER_SEC;
+    printf("Imprime: %.3f\n", time_taken);
+    
+    // ######################
+    
     Libera_Ponto(info->P, info->Contagem);
     free(info->D);
     free(info);
